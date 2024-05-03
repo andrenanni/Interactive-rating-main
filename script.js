fba5e19459ad5552d94979c4215dab96ae6662fd
andrenanni <andre-nanni@hotmail.com>
2024-05-03 14:40:44 -0300

    Ajuste no HTML e CSS e montagem do JS

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+var circles = document.querySelectorAll('.circle');
 var btn = document.querySelector('.btn');
+var boxFlip = document.querySelector('.thanks-box-flip');
 
-btn.addEventListener("click", function(){
-    alert("Hello World!");
+circles.forEach(function(circle){
+    circle.addEventListener("click", function(){
+        btn.classList.add("btn-newStyle");
+    
+        btn.addEventListener("click", function(){
+            boxFlip.style.zIndex = "1";
+        });
+    });
 });
