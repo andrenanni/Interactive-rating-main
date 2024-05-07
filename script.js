4f8bb5a0a08da3116e1b7bb142fc1f2d3097bbf4
andrenanni <andre-nanni@hotmail.com>
2024-05-07 14:52:08 -0300

    Ajuste no HTML, CSS e finalização do JS

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 var circles = document.querySelectorAll('.circle');
 var btn = document.querySelector('.btn');
 var boxFlip = document.querySelector('.thanks-box-flip');
+var rating = document.querySelector('#select-txt');
 
 circles.forEach(function(circle){
+
+let num = parseInt(circle.textContent);
+
     circle.addEventListener("click", function(){
         btn.classList.add("btn-newStyle");
     
         btn.addEventListener("click", function(){
             boxFlip.style.zIndex = "1";
+
+            var txtRating = `You selected ${num} out of 5`;
+            rating.textContent = txtRating;
         });
     });
 });
